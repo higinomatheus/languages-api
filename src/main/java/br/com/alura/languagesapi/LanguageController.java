0cae75498ea88c3a6e573af89955a7b0445e632e
Matheus Higino <matheus.higino@yahoo.com.br>
2023-04-01 07:26:37 -0300

    Adicao dos métodos PUT e DELTETE

--- src/main/java/br/com/alura/languagesapi/LanguageController.java
+++ src/main/java/br/com/alura/languagesapi/LanguageController.java
@@ -1,13 +1,17 @@
 package br.com.alura.languagesapi;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,5 +30,26 @@ public class LanguageController {
     public ResponseEntity<Language> postLanguage(@RequestBody Language language) {
         Language lang = repository.insert(language);
         return new ResponseEntity<>(lang, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/language")
+    public String putLanguage(@RequestBody Language language) {
+        Optional<Language> lang = repository.findById(language.getId());
+        if (!lang.isEmpty()) {
+            repository.save(language);
+            return "Language updated";
+        } else {
+            return "Language NOT updated";
+        }
+    }
+
+    @DeleteMapping("/language")
+    public String deleteImage(@RequestParam String id) {
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+            return "Language deleted";
+        } else {
+            return "Invalid id";
+        }
     }
 }
