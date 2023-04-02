8ea56e1260f3e1f55ae43312242fb1e9d2a44657
Matheus Higino <matheus.higino@yahoo.com.br>
2023-04-02 08:18:41 -0300

    Alterações na classe Language e inclusão do arquivo Anotações, que explica passo a passo de como criar um arquivo .jar.

--- src/main/java/br/com/alura/languagesapi/Language.java
+++ src/main/java/br/com/alura/languagesapi/Language.java
@@ -5,7 +5,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "principaisLinguagens")
 public class Language {
-
+    // Esta classe representa a collection "principaisLinguages" do MongoDB
     @Id
     private String id;
     private String title;
